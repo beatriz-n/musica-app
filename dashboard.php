a8e153038f825a18532fb7818bfa543a04dccccd
beatriz-n <beatriznespolid@gmail.com>
2024-07-23 11:17:20 -0300

    listagem dashboard (modulo aprendizagem ok)

--- dashboard.php
+++ dashboard.php
@@ -1,56 +1,89 @@
-<?php require_once 'headeraprendizagem.php'; ?>
 <?php
-//use essas classes que personalizei para demonstrar se está completa ou incompleta
-// -> exemplo de uso da classe card-focus-completa quando completa as atividades
-// -> 1 = completa; 0 = incompleta
-$completaAtividade = 0;
-if ($completaAtividade == 1) {
-    $status = "card-focus-completa";
-} else {
-    //pegar o primeiro registro incompleto para colocar a classe card-focus-atual
-    //que será "focalizada"
-    $status = "card-focus-atual";
-    $idAtual = "id='cardAtual'";
-    //as demais incompletas não terão uma "estilização" css especial
-}
-?>
-<!-- Begin Page Content -->
-<div class="container-fluid d-flex flex-column align-items-center justify-content-center overflow" style="padding: 200px;">
-    <div class="card w-50 mb-3 card-focus-completa" onclick="focusCard(this)">
-        <div class="card-body text-center">
-            Lição 1 - Notas musicais
-        </div>
-    </div>
-    <div class="card w-50 mb-3 card-focus-completa" onclick="focusCard(this)">
-        <div class="card-body text-center">
-            Lição 2 - Ritmos
-        </div>
-    </div>
-    <div class="card w-50 mb-3 card-focus-completa" onclick="focusCard(this)">
-        <div class="card-body text-center">
-            Lição 3 - Acordes
-        </div>
-    </div>
-    <div class="card w-50 mb-3 card-focus-atual" onclick="focusCard(this)">
-        <div class="card-body text-center">
-            Lição 4 - Escalas
-        </div>
-    </div>
-    <div class="card w-50 mb-3 card-focus-incompleta" onclick="focusCard(this)">
-        <div class="card-body text-center">
-            Lição 5 - Modulação
-        </div>
-    </div>
+require_once './core/conexaoDB.php';
+require_once 'headeraprendizagem.php';
 
-<!-- Barra de Progresso -->
-<div class="fixed-bottom">
-    <div class="xp-bar-label">Barra de Progresso</div>
-    <div class="xp-bar-container">
-        <div class="xp-bar">
-            <div class="xp-bar-fill" style="width: 80%;"></div> <!-- style width define o quanto a barra vai estar preenchida -->
+$cmd = "SELECT * FROM modulo";
+$result = mysqli_query($con, $cmd);
+$qtdRegistros = mysqli_num_rows($result);
+$array = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+$arrayNivel = [];
+$nivelMenor = 999;
+$qtdCompletos = 0;
+
+for ($i = 0; $i < $qtdRegistros; $i++) {
+    $dados = $array[$i];
+    $idModulo = $dados['idModulo'];
+    $nivelModulo = $dados['nivelModulo'];
+    $completoModulo = $dados['completoModulo'];
+    // acha o nivel menor incompleto
+    if (($nivelModulo < $nivelMenor) && $completoModulo == 0) {
+        $nivelMenor = $nivelModulo;
+        $idModuloAtual = $idModulo;
+    }
+
+    // criando array nivel
+    $arrayNivel[] = [
+        'id' => $idModulo,
+        'nivel' => $nivelModulo
+    ];
+}
+echo $nivelMenor;
+
+// ordena o array pelo nível
+usort($arrayNivel, function ($a, $b) {
+    return $a['nivel'] <=> $b['nivel'];
+});
+?>
+
+<div class="container-fluid d-flex flex-column align-items-center justify-content-center overflow" style="padding: 200px;">
+    <?php
+    for ($j = 0; $j < count($arrayNivel); $j++) {
+        $idModuloOrdenado = $arrayNivel[$j]['id'];
+        $nivelModuloOrdenado = $arrayNivel[$j]['nivel'];
+
+        $cmd2 = "SELECT * FROM modulo WHERE idModulo = $idModuloOrdenado";
+        $result2 = mysqli_query($con, $cmd2);
+        $array2 = mysqli_fetch_all($result2, MYSQLI_ASSOC);
+        // titulo do modulo ordenado
+        $tituloModuloOrdenado = $array2[0]['tituloModulo'];
+        // completo modulo
+        $completoModuloOrdenado = $array2[0]['completoModulo'];
+
+        // contador de niveis completos para calculo de progresso
+        if ($completoModuloOrdenado == 1) {
+            $qtdCompletos++;
+        }
+
+        // idmodulo atual é o id do modulo com o nivel menor e incompleto
+        if ($nivelModuloOrdenado == $nivelMenor) {
+            $status = "card-focus-atual";
+        } else if ($nivelModuloOrdenado > $nivelMenor) {
+            $status = "card-focus-incompleta";
+        } else if ($completoModuloOrdenado == 1) {
+            $status = "card-focus-completa";
+        }
+
+    ?>
+        <div class="card w-50 mb-3 <?= $status ?>" onclick="focusCard(this)">
+            <div class="card-body text-center">
+                Nível <?= $nivelModuloOrdenado ?> - <?= $tituloModuloOrdenado ?>
+            </div>
+        </div>
+    <?php
+    }
+    // calculo do progresso do aluno
+    $progresso = (100 * $qtdCompletos) / $qtdRegistros;
+    ?>
+    <!-- Barra de Progresso -->
+    <div class="fixed-bottom">
+        <div class="xp-bar-label">Barra de Progresso</div>
+        <div class="xp-bar-container">
+            <div class="xp-bar">
+                <div class="xp-bar-fill" style="width: <?=$progresso?>%;"></div> <!-- style width define o quanto a barra vai estar preenchida -->
+            </div>
         </div>
     </div>
-</div>
 
 </div>
 <?php require_once 'footer.php'; ?>
